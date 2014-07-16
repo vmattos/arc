b7241ed8757e4abdbcb24381d677e8b26e38adc9
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-07-16 20:46:32 -0300

    Function to extract courses urls

---
+++ src/ws/ws-utils.js
@@ -0,0 +1,17 @@
+var wsUtils = {};
+
+wsUtils.getUrls function(op) {
+  var urls = []; 
+
+  for(course in op.courses) {
+    var course = op.courses[course];
+    var url = op.alura + op.path + course + op.hash;
+    urls.push(url);
+
+    console.log(course + ' url: ' + url);
+  }
+
+  return urls;
+}
+
+module.exports = wsUtils;
