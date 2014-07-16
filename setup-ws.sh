6d4501720907f68428aa741599036561e5bc0524
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-07-16 20:33:07 -0300

    script to setup env variables

---
+++ setup-ws.sh
@@ -0,0 +1,13 @@
+echo 'Exporting env variables'
+
+# Alura url. eg: http://www.alura.com.br
+export ALURA_URL='INSERT URL'
+echo 'ALURA_URL: ' $ALURA_URL
+
+# Alura web service path. eg: /ws/tutorial
+export ALURA_PATH='INSERT PATH'
+echo 'ALURA_PATH: ' $ALURA_PATH
+
+# Alura hash (comes at the end of the url). eg: /BiUyuI9ie8Hj5i8
+export ALURA_HASH='INSERT HASH'
+echo 'ALURA_HASH: ' $ALURA_HASH
