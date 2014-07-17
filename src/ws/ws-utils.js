7fb2ee9a443f19f65f941035d28fb23ec492c2a4
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-07-16 21:30:50 -0300

    Making tests pass

--- src/ws/ws-utils.js
+++ src/ws/ws-utils.js
@@ -3,6 +3,8 @@ var wsUtils = {};
 wsUtils.getUrls =  function(op) {
   var urls = []; 
 
+  if(Object.keys(op.courses).length < 1) throw new Error('No course was found.');
+
   for(course in op.courses) {
     var course = op.courses[course];
     var url = op.alura + op.path + course + op.hash;
