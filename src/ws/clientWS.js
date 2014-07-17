8e9aca35a4123a80ee8dc05e4fcc190ffd1e6e49
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-07-16 23:45:16 -0300

    Refactoring clientWS

--- src/ws/clientWS.js
+++ src/ws/clientWS.js
@@ -1,15 +1,15 @@
 var spawn = require('child_process').spawn
   , wsUtils = require('./ws-utils.js')
-  , options = require('./ws-options');
+  , options = require('./ws-options')
+  , courses = Object.keys(options.courses);
+  , urls = wsUtils.getUrls(options);
 
-var courses = Object.keys(options.courses);
-
-var urls = wsUtils.getUrls(options);
-
+// Params to generate full AFC
 var courseParms = {
   courseName: options.courseName || courses[0],
   xmls: []
 };
+
 
 for(i in urls) {
   (function() {
