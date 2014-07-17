44d65b8caf49d20b7bd54df5471e56625890fcb8
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-07-17 19:06:16 -0300

    minor

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ courseParams.on('newXml', function() {
     });
 
     afcUtils.createDirectory(courseParams);
-    afcUtils.createAfcs(courseParams)
-;  }
+    afcUtils.createAfcs(courseParams);  
+  }
 });
