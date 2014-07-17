52d5a2eaaeed3792a3a524910bbfd11c426c8512
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-07-17 19:23:06 -0300

    Fixing italic parsing

--- src/parser/parser.js
+++ src/parser/parser.js
@@ -5,11 +5,12 @@ parser = {};
 parser.parseItalic = function(string) {
   string = md(string, true, "em");
 
-  var italicRegexp = /<em>(.*)<\/em>/g;
+  var italicRegexp = /<em>/g;
   var italics = string.match(italicRegexp);
 
   for(i in italics) {
-    string = string.replace(italicRegexp, "::$1::");
+    string = string.replace("<em>", "::");
+    string = string.replace("</em>", "::");
   }
 
   return string;
