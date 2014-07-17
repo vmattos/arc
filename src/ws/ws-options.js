976a974155495e73d5effc1c7b9d841ab646ccfd
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-07-16 21:34:55 -0300

    Course name optional param

--- src/ws/ws-options.js
+++ src/ws/ws-options.js
@@ -6,7 +6,10 @@ var wsOptions = {
   courses: {
     module1: "php-mysql-e-fundamentos-da-web",
     module2: "php-mysql-e-fundamentos-da-web-parte-2"
-  }
+  },
+
+  //Optional: Name for the full-course 
+  courseName: "php"
 }
 
 module.exports = wsOptions;
