c4500a8c39b7f41f599cdd871117c3bcc579fbb9
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-07-16 21:50:19 -0300

    Fetching XMLs with clienteWs

---
+++ src/ws/clientWS.js
@@ -0,0 +1,34 @@
+var spawn = require('child_process').spawn
+  , wsUtils = require('./ws-utils.js')
+  , options = require('./ws-options');
+
+var courses = Object.keys(options.courses);
+
+var urls = wsUtils.getUrls(options);
+
+var courseParms = {
+  courseName: options.courseName || courses[0],
+  xmls: []
+};
+
+for(i in urls) {
+  (function() {
+    var atual = i;
+    var url = urls[atual]
+    var xml = ''
+    var curl = spawn('curl', [url]);
+
+    console.log('Fetching XML for ' + courses[atual] + '(' + url + ')');
+
+    curl.stdout.on('data', function(data) {
+      xml += data;
+    });
+
+    curl.on('close', function(code) {
+      code == 0 ? 
+        console.log('Finished receiving XML for ' + courses[atual])
+        : console.log('Child process (curl) exited with code ' + 0);
+    }); 
+  })();
+}
+
