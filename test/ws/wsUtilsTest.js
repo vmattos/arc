e222b0f95690e19a016bed3eb2c18d149b922c62
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-07-16 21:18:56 -0300

    Unit testing ws-utils getUrl function

--- test/ws/wsUtilsTest.js
+++ test/ws/wsUtilsTest.js
@@ -6,4 +6,35 @@ describe('wsUtils', function() {
   it('should have getUrls function', function() {
     wsUtils.should.have.property('getUrls');
   });
+
+  describe('getUrls', function() {
+
+    var wsOptions = {}
+
+    beforeEach(function() {
+      wsOptions = {
+        alura: "http://fakealura.com.br",
+        path: "/ws/fake/",
+        hash: "/fAk3h45h",
+
+        courses: {
+        }
+      }
+    });
+
+    it('should return url given 1 course', function() {
+      wsOptions.courses.module1 = "module1";
+
+      var urls = wsUtils.getUrls(wsOptions);
+      urls.should.have.length(1);
+    });
+
+    it('should return urls given 2 courses', function() {
+      wsOptions.courses.module1 = "module1";
+      wsOptions.courses.module2 = "module2";
+
+      var urls = wsUtils.getUrls(wsOptions);
+      urls.should.have.length(2);
+    });
+  });
 });
