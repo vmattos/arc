c51b190fb738b5bcbd066d8daefd47321937d22d
Rômulo Henrique <romulo.henriquemv@gmail.com>
2016-06-28 16:22:52 -0300

    Update setup-ws.sh

--- setup-ws.sh
+++ setup-ws.sh
@@ -1,11 +1,11 @@
 echo 'Exporting env variables'
 
-# Alura url. eg: http://www.alura.com.br
-export ALURA_URL='INSERT URL'
+# Alura url. eg: https://cursos.alura.com.br
+export ALURA_URL='https://cursos.alura.com.br'
 echo 'ALURA_URL: ' $ALURA_URL
 
-# Alura web service path. eg: /ws/tutorial/
-export ALURA_PATH='INSERT PATH'
+# Alura web service path. eg: /webservice/course/
+export ALURA_PATH='/webservice/course/'
 echo 'ALURA_PATH: ' $ALURA_PATH
 
 # Alura hash (comes at the end of the url). eg: /BiUyuI9ie8Hj5i8
